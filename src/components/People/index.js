d178f878b3d0d79e499ad8b52961d0efbc7ec835
Angela <angela@softwareseni.com>
2018-08-18 17:21:52 +0700

    Add infinite scroll

--- src/components/People/index.js
+++ src/components/People/index.js
@@ -4,23 +4,61 @@ import { Link } from 'react-router-dom';
 
 import './style.css';
 class People extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasMore: true,
+            iteration: 1,
+            perScroll: 10
+        }
+        this.onScroll = this.onScroll.bind(this);
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll, false);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll, false);
+    }
+    onScroll() {
+        let windowHeight = window.innerHeight,
+            windowScrollY = window.scrollY,
+            documentHeight = document.body.offsetHeight;
+        if((windowScrollY + windowHeight) >= documentHeight && this.state.hasMore) {
+            console.log('yeah DONG');
+            this.setState((prevState) => ({
+                iteration: prevState.iteration + 1
+            }))
+            if(this.state.hasMore && this.props.peopleIsUpdated && this.props.people.length < (this.state.iteration*this.state.perScroll)) {
+                this.setState({
+                    hasMore: false,
+                    iteration: 1
+                })
+            }
+        } 
+    }
     render() { 
         let data;
-        
+
         if(this.props.isFiltered) {
             data = this.props.filter;
         } else {
             data = this.props.people;
         }
-
+        console.log(data.length,'panjang data sblm');
+        if(data.length > 10 && this.state.hasMore) {
+            data = data.slice((this.state.iteration - 1 ), (this.state.iteration * this.state.perScroll));
+        }
+        console.log(data.length,'panjang data');
         return (
             <div className="row">
                 {
                     (this.props.peopleIsUpdated == true) ? (
                         (data.length > 0) ? (
                             Object.keys(data).map((key) => (
-                                <div key={key} className="col-12 color-white">
-                                    <Link to={'people/'+(data[key].name).toLowerCase().replace(' ','-')} className="color-white">
+                                <div key={key} className="col-12">
+                                    <Link to={'people/'+(data[key].name).toLowerCase().replace(' ','-')} className="color-white color-yellow-hover text-decoration-none transition-2-ease">
                                         <div className="row justify-content-center mb-3">
                                             <div className="col-lg-4 col-12">
                                                 <p className="mb-0 font-starjedi border-bottom border-top pt-3 pb-3">Character Name</p>
