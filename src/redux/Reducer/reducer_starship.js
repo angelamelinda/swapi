a9fc6ff366f51aaf33c883435da361e10f25d18c
Angela <angela@softwareseni.com>
2018-08-18 12:02:04 +0700

    Fix typo in starship reducer

--- src/redux/Reducer/reducer_starship.js
+++ src/redux/Reducer/reducer_starship.js
@@ -2,11 +2,11 @@ const initialState = { isUpdating: false, isUpdated: false, starship: [] }
 
 export function Starship(state = initialState, action) {
   switch (action.type) {
-    case 'STARTSHIP_UPDATING':
+    case 'STARSHIP_UPDATING':
       return {...state, isUpdating: true, isUpdated: false}
-    case 'STARTSHIP_UPDATED':
+    case 'STARSHIP_UPDATED':
       return {...state, isUpdating: false, isUpdated: true, starship: action.payload}
-    case 'STARTSHIP_UPDATE_FAILED':
+    case 'STARSHIP_UPDATE_FAILED':
       return {...state, isUpdating: false, isUpdated: false, starship: []}
     default:
       return state
