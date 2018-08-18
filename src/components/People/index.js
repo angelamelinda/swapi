481db9508e4dad91f0ec430403bef0e8178ab8d0
Angela <angela@softwareseni.com>
2018-08-18 12:25:48 +0700

    Update people view

--- src/components/People/index.js
+++ src/components/People/index.js
@@ -20,8 +20,71 @@ class People extends Component {
                         (data.length > 0) ? (
                             Object.keys(data).map((key) => (
                                 <div key={key} className="col-12 color-white">
-                                    <Link to={'people/'+(data[key].name).toLowerCase().replace(' ','-')}>
-                                        {data[key].name}
+                                    <Link to={'people/'+(data[key].name).toLowerCase().replace(' ','-')} className="color-white">
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-starjedi border-bottom border-top pt-3 pb-3">Character Name</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 border-bottom border-top pt-3 pb-3 pl-3 pr-3">{data[key].name}</p>
+                                            </div>
+                                        </div>  
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-weight-bold">Gender</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 pl-3 pr-3">{data[key].gender}</p>
+                                            </div>
+                                        </div>
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-weight-bold">Birth Year</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 pl-3 pr-3">{data[key].birth_year}</p>
+                                            </div>
+                                        </div>
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-weight-bold">Height</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 pl-3 pr-3">{data[key].height} cm</p>
+                                            </div>
+                                        </div>
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-weight-bold">Mass</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 pl-3 pr-3">{data[key].mass} kg</p>
+                                            </div>
+                                        </div>
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-weight-bold">Eye Color</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 pl-3 pr-3">{data[key].eye_color}</p>
+                                            </div>
+                                        </div>
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-weight-bold">Hair Color</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 pl-3 pr-3">{data[key].hair_color}</p>
+                                            </div>
+                                        </div>
+                                        <div className="row justify-content-center mb-3">
+                                            <div className="col-lg-4 col-12">
+                                                <p className="mb-0 font-weight-bold">Skin Color</p>
+                                            </div>
+                                            <div className="col-lg-8 col-12">
+                                                <p className="mb-0 pl-3 pr-3">{data[key].skin_color}</p>
+                                            </div>
+                                        </div>
                                     </Link>
                                 </div>
                             ))
