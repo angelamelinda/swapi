f77a4a565fce839c90afdd6cac76dd1e457799ec
Angela <angela@softwareseni.com>
2018-08-19 01:44:24 +0700

    Create a function to reduce redundant action

--- src/containers/DetailPeople/index.js
+++ src/containers/DetailPeople/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import queryString  from 'query-string';
+
 import Hero from '../../assets/images/starwars.jpg';
-import './style.css'
 
 class DetailPeople extends Component {
+    constructor(props) {
+        super(props);
+        this.matchData = this.matchData.bind(this);
+    }
+    matchData(specificData, peopleData) {
+        let result = [];
+        Object.keys(specificData).map((key) => {
+            peopleData.map((data) => {
+                if(specificData[key].url == data) {
+                    result.push(specificData[key]);
+                }
+            })
+        })
+        return result;
+    }
     render() {
         let people = {}, 
             peopleFilm = [], 
@@ -13,54 +29,26 @@ class DetailPeople extends Component {
             peopleSpecies = [],
             peopleStarship = [];
         if(this.props.peopleIsUpdated) {
-            let path = (this.props.match.params.name).replace('-',' ');
+            let path = queryString.parse(this.props.location.search).name;
             let idx = Object.keys(this.props.people).filter((key) => {
-                let name = (this.props.people[key].name).toLowerCase().replace('-',' ');
+                let name = this.props.people[key].name;
                 return name == path
             })
-
             if(idx.length != 0) {
                 people = this.props.people[idx];
-                Object.keys(this.props.film).map((key) => {
-                    people.films.map((film) => {
-                        if(this.props.film[key].url == film) {
-                            peopleFilm.push(this.props.film[key]);
-                        }
-                    })
-                })
-
-                Object.keys(this.props.species).map((key) => {
-                    people.species.map((type) => {
-                        if(this.props.species[key].url == type) {
-                            peopleSpecies.push(this.props.species[key]);
-                        }
-                    })
-                })
-
-                Object.keys(this.props.vehicle).map((key) => {
-                    people.vehicles.map((vehicle) => {
-                        if(this.props.vehicle[key].url == vehicle) {
-                            peopleVehicle.push(this.props.vehicle[key]);
-                        }
-                    })
-                })
-
-                Object.keys(this.props.starship).map((key) => {
-                    people.starships.map((ship) => {
-                        if(this.props.starship[key].url == ship) {
-                            peopleStarship.push(this.props.starship[key]);
-                        }
-                    })
-                })
+                
+                peopleFilm = this.matchData(this.props.film, people.films);
+                peopleSpecies = this.matchData(this.props.species, people.species);
+                peopleVehicle = this.matchData(this.props.vehicle, people.vehicles);
+                peopleStarship = this.matchData(this.props.starship, people.starships);
 
                 Object.keys(this.props.planet).map((key) => {
                     if(this.props.planet[key].url == people.homeworld) {
                         peopleHomeworld.push(this.props.planet[key].name);
                     }
                 })
-                console.log(peopleStarship)
             } else {
-                return <Redirect to="/404" />
+                return <Redirect to="/notfound" />
             }
         } 
 
@@ -75,127 +63,128 @@ class DetailPeople extends Component {
                 {
                     this.props.peopleIsUpdated && (
                         <div className="container pt-5 pb-5 color-white">
+                            <button className="mb-4" onClick={this.props.history.goBack}>Back</button>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-starjedi border-bottom border-top pt-3 pb-3">Character Name</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 border-bottom border-top pt-3 pb-3 pl-3 pr-3">{people.name}</p>
                                 </div>
                             </div>  
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Gender</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 pl-3 pr-3">{people.gender}</p>
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Birth Year</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 pl-3 pr-3">{people.birth_year}</p>
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Height</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 pl-3 pr-3">{people.height} cm</p>
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Mass</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 pl-3 pr-3">{people.mass} kg</p>
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Eye Color</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 pl-3 pr-3">{people.eye_color}</p>
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Hair Color</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 pl-3 pr-3">{people.hair_color}</p>
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Skin Color</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-0 pl-3 pr-3">{people.skin_color}</p>
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Film(s)</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     {
                                         peopleFilm.length > 0 ?
                                         Object.keys(peopleFilm).map((key) => (
-                                            <p className="mb-2 pl-3 pr-3">{peopleFilm[key].title}</p>
-                                        )) : '-'
+                                            <p className="mb-2 pl-3 pr-3" key={key}>{peopleFilm[key].title}</p>
+                                        )) : <p className="mb-2 pl-3 pr-3">-</p>
                                     }
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Vehicle(s)</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     {
                                         peopleVehicle.length > 0 ?
                                         Object.keys(peopleVehicle).map((key) => (
-                                            <p className="mb-2 pl-3 pr-3">{peopleVehicle[key].name}</p>
-                                        )) : '-'
+                                            <p className="mb-2 pl-3 pr-3" key={key}>{peopleVehicle[key].name}</p>
+                                        )) : <p className="mb-2 pl-3 pr-3">-</p>
                                     }
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Homeworld</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     <p className="mb-2 pl-3 pr-3">{peopleHomeworld[0]}</p>
                                 </div>
                             </div> 
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Species</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     {
                                         peopleSpecies.length > 0 ?
                                         Object.keys(peopleSpecies).map((key) => (
-                                            <p className="mb-2 pl-3 pr-3">{peopleSpecies[key].name}</p>
-                                        )) : '-'
+                                            <p className="mb-2 pl-3 pr-3" key={key}>{peopleSpecies[key].name}</p>
+                                        )) : <p className="mb-2 pl-3 pr-3">-</p>
                                     }
                                 </div>
                             </div>
                             <div className="row justify-content-center mb-3">
-                                <div className="col-lg-4 col-12">
+                                <div className="col-lg-4 col-6">
                                     <p className="mb-0 font-weight-bold">Starship(s)</p>
                                 </div>
-                                <div className="col-lg-8 col-12">
+                                <div className="col-lg-8 col-6">
                                     {
                                         peopleStarship.length > 0 ?
                                         Object.keys(peopleStarship).map((key) => (
-                                            <p className="mb-2 pl-3 pr-3">{peopleStarship[key].name}</p>
-                                        )) : '-'
+                                            <p className="mb-2 pl-3 pr-3"key={key}>{peopleStarship[key].name}</p>
+                                        )) : <p className="mb-2 pl-3 pr-3">-</p>
                                     }
                                 </div>
                             </div>       
@@ -210,25 +199,24 @@ class DetailPeople extends Component {
 const mapStateToProps = (state) => {
     return {
         film: state.Film.film,
-        filmIsUpdating: state.Film.isUpdating,
         filmIsUpdated: state.Film.isUpdated,
+        
         people: state.People.people,
-        peopleIsUpdating: state.People.isUpdating,
         peopleIsUpdated: state.People.isUpdated,
+        
         planet: state.Planet.planet,
-        planetIsUpdating: state.Planet.isUpdating,
         planetIsUpdated: state.Planet.isUpdated,
+        
         species: state.Species.species,
-        speciesIsUpdating: state.Species.isUpdating,
         speciesIsUpdated: state.Species.isUpdated,
+        
         starship: state.Starship.starship,
-        starshipIsUpdating: state.Starship.isUpdating,
         starshipIsUpdated: state.Starship.isUpdated,
+        
         vehicle: state.Vehicle.vehicle,
-        vehicleIsUpdating: state.Vehicle.isUpdating,
         vehicleIsUpdated: state.Vehicle.isUpdated,
+        
         filter: state.Filter.filter,
-        isFiltering: state.Filter.isFiltering,
         isFiltered: state.Filter.isFiltered
     }
   }
